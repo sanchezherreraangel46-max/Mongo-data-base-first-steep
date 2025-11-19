d1841a69de228440517d171ad6df2709d68d49f7
IsBatman777 <sanchez.herrera.angel.46@cbtis118.edu.mx>
2025-11-19 17:54:16 +0000

    se agrego practica1.mongodb.js

---
+++ practica1.mongodb.js
@@ -0,0 +1,7 @@
+use("DataqbaseEscolar")
+
+db.createCollection("alumnos");
+db.createCollection("eventos");
+db.createCollection("plantel");
+db.createCollection("escuela");
+db.createCollection("profesores");
